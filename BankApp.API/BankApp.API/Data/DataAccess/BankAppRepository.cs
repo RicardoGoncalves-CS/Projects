425cf29d56d8849dd2ee26dc19001cb2af382e02
RicardoGoncalves-CS <126096527+RicardoGoncalves-CS@users.noreply.github.com>
2023-04-27 21:19:30 +0100

    Added Data Access Layer to BankApp.API

---
+++ BankApp.API/BankApp.API/Data/DataAccess/BankAppRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankApp.API.Data.DataAccess
+{
+    public class BankAppRepository<T> : IBankAppRepository<T> where T : class
+    {
+        private readonly DataContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BankAppRepository(DataContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public bool IsNull => throw new NotImplementedException();
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _dbSet.AddRange(entities);
+        }
+
+        public async Task<T?> FindAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
---
+++ BankApp.API/BankApp.API/Data/DataAccess/IBankAppRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace BankApp.API.Data.DataAccess
+{
+    public interface IBankAppRepository<T>
+    {
+        bool IsNull { get; }
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> FindAsync(int id);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        Task SaveAsync();
+    }
+}
