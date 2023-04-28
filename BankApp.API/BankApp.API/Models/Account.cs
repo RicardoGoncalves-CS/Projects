a0a9f6dddab8687af7c3f733c1f7c97eda9d8657
RicardoGoncalves-CS <126096527+RicardoGoncalves-CS@users.noreply.github.com>
2023-04-28 13:13:25 +0100

    Deleted BankApp

--- BankApp.API/BankApp.API/Models/Account.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BankApp.API.Models
-{
-    public class Account
-    {
-        public int Id { get; set; }
-        public decimal Balance { get; set; }
-        public Customer Customer { get; set; }
-    }
-}
