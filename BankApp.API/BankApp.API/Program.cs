be392a4baa49a4a362172aee2d73828af0ba2d82
RicardoGoncalves-CS <126096527+RicardoGoncalves-CS@users.noreply.github.com>
2023-05-02 11:46:46 +0100

    Added Customer and Account controllers

--- BankApp.API/BankApp.API/Program.cs
+++ BankApp.API/BankApp.API/Program.cs
@@ -7,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(opt => opt.SerializerSettings
+    .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
